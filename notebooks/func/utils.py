e9efe64cdcfc1b5ca8b026049727b5dcfa734a77
xpcosmos <mikeias.d.s.o@gmail.com>
2023-09-08 19:07:46 -0300

    Training with transformers - Roberta

--- notebooks/func/utils.py
+++ notebooks/func/utils.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
-def read_data():
-    return pd.read_csv('../data/raw_data/tweet-sentiment-extraction/train.csv')
+def read_data(data):
+    if data == 'sia':
+        return pd.read_csv('../data/processed_data/twitter/df_sia.csv')
+    elif data == 'original' or not data:
+        return pd.read_csv('../data/raw_data/tweet-sentiment-extraction/train.csv')
+    
 
 def save_processed_csv(df, folder, filename):
     df.to_csv(f'../data/processed_data/{folder}/{filename}.csv', index=False)
